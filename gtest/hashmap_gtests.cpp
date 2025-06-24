a772269ce2a06298d6d262c0e978cb814b7fada1
Noah Wang <bwang.noah@gmail.com>
2025-06-23 17:39:46 -0700

    implement hashmap default constructor/destructor and gtests

--- gtest/hashmap_gtests.cpp
+++ gtest/hashmap_gtests.cpp
@@ -1,11 +1,14 @@
 #include <gtest/gtest.h>
+#include <string>
 
 #include "hashmap.hpp"
+#include "user.hpp"
 
 namespace {
     
     TEST(HashMapInitialization, DefaultConstructor)
     {
+        HashMap<std::string, User> map;
         EXPECT_TRUE(false);
     }
 }
--- src/hashmap.hpp
+++ src/hashmap.hpp
@@ -2,11 +2,14 @@
 #define HASHMAP_HPP
 
 #include <iostream>
+#include <sstream>
 #include <functional>
 
 #include "linkedlist.hpp"
 
-template <typename DataType>
+template <typename KeyType, typename DataType>
+// DataType synonymous with a hashmaps value
+
 class HashMap
 {
     static constexpr double loadFactor{ 0.8 };
@@ -14,25 +17,28 @@ class HashMap
     unsigned int numBuckets;
     LinkedList<DataType>** map;
 
-    std::function<unsigned int(const std::string&)>hashFunction;
+    std::function<unsigned int(const KeyType&)>hashFunction;
 
+    void deleteMap() noexcept;
     void rehash();
+
 public:
     
     // default constructor
     HashMap() noexcept;
 
     // copy constructor
-    HashMap(std::function<unsigned int(const std::string&)> hashFunction);
+    HashMap(std::function<unsigned int(const KeyType&)> hashFunction);
 
-    const std::string& get(const std::string& key) const;
-    void insert(const std::string& key, const std::string& value);
-    void remove(const std::string& key);
+    const DataType& get(const KeyType& key) const;
+    void insert(const KeyType& key, const DataType& value);
+    void remove(const KeyType& key);
 
-    std::string& operator[](const std::string&);
+    DataType& operator[](const KeyType&);
+    const DataType& operator[](const KeyType&) const;
 
     // copy assignment
-    HashMap& operator=(const HashMap& map);  
+    HashMap& operator=(const HashMap& map);
     bool operator==(const HashMap& map) const;
 
     bool equals(const HashMap& map) const;
@@ -43,5 +49,49 @@ public:
    // destructor 
    ~HashMap() noexcept;
 };
+
+
+template <typename KeyType, typename DataType>
+HashMap<KeyType, DataType>::HashMap() noexcept
+    : numBuckets{ 10 }
+{
+    map = new LinkedList<DataType>*[numBuckets];
+
+    for (unsigned int i{ 0 }; i < numBuckets; ++i) {
+        map[i] = new LinkedList<DataType>();
+    }
+}
+
+
+template <typename KeyType, typename DataType>
+std::string HashMap<KeyType, DataType>::toString() const
+{
+    std::ostringstream oss;
+    for (unsigned int i{ 0 }; i < numBuckets; ++i) {
+        // TODO
+    }
+
+    return oss.str();
+}
+
+template <typename KeyType, typename DataType>
+void HashMap<KeyType, DataType>::deleteMap() noexcept
+{
+    if (map == nullptr)
+        return;
+
+    for (unsigned int i{ 0 }; i < numBuckets; ++i) {
+        delete map[i];
+        map[i] = nullptr;
+    }
+    delete[] map;
+    map = nullptr;
+}
+
+template <typename KeyType, typename DataType>
+HashMap<KeyType, DataType>::~HashMap() noexcept
+{
+    deleteMap(); 
+}
 
 #endif
